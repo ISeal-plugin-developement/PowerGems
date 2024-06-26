be1d73753ae292ba5a710e16132b0582ce94276f
ISeal <69968405+willyema@users.noreply.github.com>
2024-06-26 22:31:51 +0200

    Fix ConfigManager

--- src/main/java/me/iseal/powergems/managers/ConfigManager.java
+++ src/main/java/me/iseal/powergems/managers/ConfigManager.java
@@ -2,6 +2,7 @@ package me.iseal.powergems.managers;
 
 import me.iseal.powergems.managers.Configuration.ActiveGemsConfigManager;
 import me.iseal.powergems.managers.Configuration.CooldownConfigManager;
+import me.iseal.powergems.managers.Configuration.GemMaterialConfigManager;
 import me.iseal.powergems.managers.Configuration.GeneralConfigManager;
 
 public class ConfigManager {
@@ -9,11 +10,13 @@ public class ConfigManager {
     private final GeneralConfigManager generalConfigManager = new GeneralConfigManager();
     private final ActiveGemsConfigManager activeGemsConfigManager = new ActiveGemsConfigManager();
     private final CooldownConfigManager cooldownConfigManager = new CooldownConfigManager();
+    private final GemMaterialConfigManager gemMaterialConfigManager = new GemMaterialConfigManager();
 
     public void setUpConfig() {
         generalConfigManager.setUpConfig();
         activeGemsConfigManager.setUpConfig();
         cooldownConfigManager.setUpConfig();
+        gemMaterialConfigManager.setUpConfig();
     }
 
     public GeneralConfigManager getGeneralConfigManager() {
@@ -28,10 +31,15 @@ public class ConfigManager {
         return cooldownConfigManager;
     }
 
+    public GemMaterialConfigManager getGemMaterialConfigManager() {
+        return gemMaterialConfigManager;
+    }
+
     public void resetConfig() {
         generalConfigManager.resetConfig();
         activeGemsConfigManager.resetConfig();
         cooldownConfigManager.resetConfig();
+        gemMaterialConfigManager.resetConfig();
     }
 
 }
