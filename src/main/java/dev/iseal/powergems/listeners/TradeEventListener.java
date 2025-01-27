10cd2da2c915179236d99b85b1bee5c1d9027eb3
ISeal <iseal@iseal.dev>
2025-01-27 17:53:23 +0100

    Refactoring, change version, add comments

--- src/main/java/dev/iseal/powergems/listeners/TradeEventListener.java
+++ src/main/java/dev/iseal/powergems/listeners/TradeEventListener.java
@@ -1,12 +1,17 @@
 package dev.iseal.powergems.listeners;
 
+import java.util.AbstractCollection;
 import java.util.Arrays;
 import java.util.List;
 
+import org.bukkit.entity.AbstractVillager;
+import org.bukkit.entity.Villager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.VillagerAcquireTradeEvent;
+import org.bukkit.event.entity.VillagerReplenishTradeEvent;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.MerchantInventory;
 
@@ -31,7 +36,7 @@ public class TradeEventListener implements Listener {
         if (!(event.getInventory() instanceof MerchantInventory)) {
             return;
         }
-        
+
         MerchantInventory inventory = (MerchantInventory) event.getInventory();
         if (hasGemInIngredients(Arrays.asList(inventory.getItem(0), inventory.getItem(1)))) {
             event.setCancelled(true);
@@ -41,6 +46,9 @@ public class TradeEventListener implements Listener {
 
     @EventHandler
     public void onVillagerAcquireTrade(VillagerAcquireTradeEvent event) {
+        //TODO: im pretty sure this causes a visual glitch with the villager
+        // upgrading, but it doesnt really upgrade it so its fine .. i guess
+        // -ISeal
         if (hasGemInIngredients(event.getRecipe().getIngredients())) {
             event.setCancelled(true);
         }
--- src/main/java/dev/iseal/powergems/listeners/passivePowerListeners/DebuffInColdBiomesListener.java
+++ src/main/java/dev/iseal/powergems/listeners/passivePowerListeners/DebuffInColdBiomesListener.java
@@ -4,6 +4,7 @@ import dev.iseal.powergems.PowerGems;
 import dev.iseal.powergems.managers.GemManager;
 import dev.iseal.powergems.managers.NamespacedKeyManager;
 import dev.iseal.powergems.managers.SingletonManager;
+import dev.iseal.powergems.misc.AbstractClasses.GemSpecificListener;
 import dev.iseal.powergems.misc.Utils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -14,42 +15,25 @@ import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.ArrayBlockingQueue;
 
-public class DebuffInColdBiomesListener implements Listener {
+public class DebuffInColdBiomesListener extends GemSpecificListener {
 
     private final Utils utils = SingletonManager.getInstance().utils;
-    private final GemManager gm = SingletonManager.getInstance().gemManager;
-    private final NamespacedKeyManager nkm = SingletonManager.getInstance().namespacedKeyManager;
-    private final List<String> allowedGems = List.of("Fire", "Lava");
-    private final ArrayBlockingQueue<UUID> slowedPlayers = new ArrayBlockingQueue<>(5);
 
-    private boolean playerHasAllowedGem(Player plr) {
-        return gm.getPlayerGems(plr).stream()
-                .anyMatch(i -> allowedGems.contains(i.getItemMeta().getPersistentDataContainer().get(nkm.getKey("gem_power"), PersistentDataType.STRING)));
+    public DebuffInColdBiomesListener() {
+        super(List.of("Fire", "Lava"), 60);
     }
 
-    @EventHandler
-    public void onMove(PlayerMoveEvent e) {
-        if (e.getFrom().getBlock() == e.getTo().getBlock()) {
-            return;
-        }
-        if (slowedPlayers.contains(e.getPlayer().getUniqueId())) {
-            return;
-        }
+    @Override
+    public boolean applyEffect(PlayerMoveEvent e, int allowedGemNumber) {
         Player plr = e.getPlayer();
-        if (!playerHasAllowedGem(plr))
-            return;
         if (plr.getLocation().getBlock().getTemperature() <= 0.05) {
-            utils.addPreciseEffect(plr, PotionEffectType.SLOW, 60, 1);
-            slowedPlayers.add(plr.getUniqueId());
-            new BukkitRunnable() {
-                @Override
-                public void run() {
-                    slowedPlayers.remove(plr.getUniqueId());
-                }
-            }.runTaskLater(PowerGems.getPlugin(), 60);
+            utils.addPreciseEffect(plr, PotionEffectType.SLOW, 60, allowedGemNumber);
+            return true;
         }
+        return false;
     }
 }
