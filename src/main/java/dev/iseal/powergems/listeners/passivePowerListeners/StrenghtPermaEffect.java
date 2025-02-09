2779f0239f4f172e6ea6de30e4caf13250c192bd
leonard-new <leon.grasic.star@gmail.com>
2025-02-09 17:17:59 +0100

    Update StrenghtPermaEffect.java

--- src/main/java/dev/iseal/powergems/listeners/passivePowerListeners/StrenghtPermaEffect.java
+++ src/main/java/dev/iseal/powergems/listeners/passivePowerListeners/StrenghtPermaEffect.java
@@ -7,7 +7,10 @@ import java.util.UUID;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+<<<<<<< Updated upstream
 import org.bukkit.event.player.PlayerInteractEvent;
+=======
+>>>>>>> Stashed changes
 import org.bukkit.persistence.PersistentDataType;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -23,6 +26,7 @@ public class StrenghtPermaEffect implements Listener {
     private final List<String> allowedGems = List.of("Strenght");
     public static final ArrayList<UUID> hasGemRaining = new ArrayList<>();
 
+<<<<<<< Updated upstream
     private void applyEffect(Player plr, int duration, int amplifier) {
         plr.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, duration, amplifier));
     }
@@ -40,4 +44,22 @@ public class StrenghtPermaEffect implements Listener {
             applyEffect(plr, 100, 1);
         }
     }
+=======
+private void applyEffect(Player plr, int duration, int amplifier) {
+    plr.addPotionEffect(new PotionEffect(PotionEffectType.INCREASE_DAMAGE, duration, amplifier));
+    }
+
+
+    private boolean playerHasAllowedGem(Player plr) {
+return gm.getPlayerGems(plr).stream()
+        .anyMatch(i -> allowedGems.contains(i.getItemMeta().getPersistentDataContainer().get(nkm.getKey("gem_power"), PersistentDataType.STRING)));
+    }
+
+    @EventHandler
+    public void applyStrenght(Player plr) {
+        if (playerHasAllowedGem(plr)) {
+            applyEffect(plr, 100, 1);
+            }
+        }   
+>>>>>>> Stashed changes
 }
