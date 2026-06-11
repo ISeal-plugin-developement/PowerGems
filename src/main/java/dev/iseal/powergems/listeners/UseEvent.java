948b62da2d47e290fa0fa8bf813252d6b8ffb551
iseal <iseal@iseal.dev>
2026-06-11 11:10:24 +0200

    fix abilities getting triggered twice when interacting with a block

--- src/main/java/dev/iseal/powergems/listeners/UseEvent.java
+++ src/main/java/dev/iseal/powergems/listeners/UseEvent.java
@@ -1,16 +1,23 @@
 package dev.iseal.powergems.listeners;
 
+import dev.iseal.powergems.PowerGems;
 import dev.iseal.powergems.managers.Configuration.ActiveGemsConfigManager;
 import dev.iseal.powergems.managers.GemManager;
 import dev.iseal.powergems.managers.SingletonManager;
 import dev.iseal.powergems.managers.TempDataManager;
 import dev.iseal.sealLib.Systems.I18N.I18N;
+import net.royawesome.jlibnoise.module.combiner.Power;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class UseEvent implements Listener {
 
@@ -19,11 +26,15 @@ public class UseEvent implements Listener {
     private final ActiveGemsConfigManager agcm = sm.configManager.getRegisteredConfigInstance(ActiveGemsConfigManager.class);
     private final GemManager gm = sm.gemManager;
 
+    // hacky fix for double trigger
+    private final List<UUID> recentlyUsed = new ArrayList<>();
+
     @EventHandler
     public void onUse(PlayerInteractEvent e) {
         if (e.getAction().equals(Action.PHYSICAL)) {
             return;
         }
+        if (recentlyUsed.contains(e.getPlayer().getUniqueId())) return;
         Player player = e.getPlayer();
         ItemStack offHandItem = player.getInventory().getItemInOffHand();
         ItemStack mainHandItem = player.getInventory().getItemInMainHand();
@@ -35,6 +46,9 @@ public class UseEvent implements Listener {
         } else {
             return;
         }
+
+        recentlyUsed.add(e.getPlayer().getUniqueId());
+        Bukkit.getScheduler().runTaskLater(PowerGems.getPlugin(), () -> recentlyUsed.remove(e.getPlayer().getUniqueId()), 1);
 
         if (tdm.cantUseGems.containsKey(player)) {
             if (System.currentTimeMillis() < tdm.cantUseGems.get(player)) {
